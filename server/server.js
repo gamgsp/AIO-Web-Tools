8f7454b3d67be9c96d2d2252171156bab62735d9
Global Ads Media <116465160+gamgsp@users.noreply.github.com>
2025-01-25 05:44:51 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(cors());
 app.use(express.json()); //
 
 // Serve static files from the dist directory
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 const subdomains = [
   'www', 'mail', 'ftp', 'remote', 'blog', 'webmail', 'server', 'ns1', 'ns2', 'smtp', 'secure', 'vpn', 'm',
@@ -713,7 +713,7 @@ schedule.scheduleJob('0 * * * *', () => {
   });
 });
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
